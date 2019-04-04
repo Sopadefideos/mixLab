3f6ee995477ff87bc79a7675218cbfd49cdad096
Antonio Marfil <antoniomarfil@MacBook-Pro-de-Antonio.local>
2019-04-04 22:28:16 +0200

    mixLab

--- index.php
+++ index.php
@@ -1,19 +1,57 @@
 <!DOCTYPE HTML>
 <html lang="ES">
 <head>
-<title>MixLab</title>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<script src="js/bootstrap.min.js"></script>
-<script src="js/jquery.js"></script>
-<link rel="stylesheet" href="css/bootstrap.min.css">
+  <title>MixLab</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="css/estilosMixLab.css">
 </head>
 
 <body>
+  <!--CABEZERA-->
+  <div>
+    <a href="index.php"><img src="img/MixLablogo.png" class="logoTam"></a>
+  </div>
 
-    <div class="container">
-      <h1>hola</h1>
-      <button class="btn btn-primary">boton</button>
+
+  <!--BUSCADOR CON BOTON-->
+  <div class="buscadorColor">
+    <nav class="navbar navbar-light">
+      <form class="form-inline" method="post">
+        <input class="form-control mr-sm-2" type="search" placeholder="Fabrik" aria-label="Fabrik">
+        <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Buscar</button>
+      </form>
+    </nav>
+  </div>
+
+
+  <!--IMAGENES SLIDER-->
+  <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img class="d-block w-100" src="https://media.watchthedj.com/djs/marcos-in-dub.jpg" alt="First slide">
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100" src="http://growsoundmag.com/wp-content/uploads/2018/12/Richie-Hawting-1.jpg" alt="Second slide">
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100" src="https://i.pinimg.com/originals/3f/16/7b/3f167b889338148e60467905481b9a99.jpg" alt="Third slide">
+      </div>
     </div>
+    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+
+
 </body>
 </html>
