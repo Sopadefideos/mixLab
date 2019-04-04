8754fe134f60f54cf6072cbd03feac76b1658013
Antonio Marfil <antoniomarfil@MacBook-Pro-de-Antonio.local>
2019-04-04 20:24:37 +0200

    mixLab

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>
+<html lang="ES">
+<head>
+<title>MixLab</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.js"></script>
+<link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+
+<body>
+
+    <div class="container">
+      <h1>hola</h1>
+      <button class="btn btn-primary">boton</button>
+    </div>
+</body>
+</html>
